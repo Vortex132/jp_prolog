049ebe27f834e01cb5fc302f4b0c29535e138961
al <33kkaarr@gmail.com>
2024-01-18 12:31:59 +0100

    added main

--- 5_7_9.pl
+++ 5_7_9.pl
@@ -1,5 +1,9 @@
+main :-
+    Plansza = [1, 2, 0],
+    czy_wygrywa(Plansza, Odp),
+    write(Odp), nl.
+
 czy_wygrywa(Plansza, false) :-
     length(Plansza, Len),
     Len < 4,
     write(Plansza), nl.
-    
